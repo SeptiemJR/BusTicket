3d7879341b2718d86c5175d5953fb0ae55298f00
SeptiemJR <30505988+SeptiemJR@users.noreply.github.com>
2017-07-28 01:05:30 +0700

    Fist time check in

---
+++ BusTicket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BusTicket
+//
+//  Created by Vivich Ratanamolanon on 7/28/2560 BE.
+//  Copyright © 2560 Vivich Ratanamolanon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
