beeb8afce927aabd1a7544cb66accae615465201
SeptiemJR <30505988+SeptiemJR@users.noreply.github.com>
2017-07-28 01:13:14 +0700

    2 Page
    2 Page

---
+++ BusTicket/SecondViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SecondViewController.swift
+//  BusTicket
+//
+//  Created by Vivich Ratanamolanon on 7/28/2560 BE.
+//  Copyright © 2560 Vivich Ratanamolanon. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func ClickBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
